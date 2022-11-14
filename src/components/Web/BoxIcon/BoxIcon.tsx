45c64c8bbdd867f02346a5fe14863733b9c4072f
RoniJackVituli <rjvtechnology@gmail.com>
2022-11-14 15:20:02 +0300

    add icons

--- src/components/Web/BoxIcon/BoxIcon.tsx
+++ src/components/Web/BoxIcon/BoxIcon.tsx
@@ -1,5 +1,6 @@
 import React  from "react";
 import classes from "./BoxIcon.module.scss";
+import core from '../../../helpers/icons/programming/css3-color.svg';
 type Props = {
   title: string;
   list: any;
@@ -10,7 +11,7 @@ const BoxIcon: React.FC<Props> = ({ title, iconTitle, list }) => {
   return (
     <div className={classes.boxiconContainer}>
       <div className={classes.titlebox}>
-        <h1>{title}</h1>
+        <h1><img src={core}/>{title}</h1>
       </div>
       
     </div>
