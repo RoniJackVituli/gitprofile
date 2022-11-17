65e8ec80c7afdbd8267c381f2873bb0776531b77
RoniJackVituli <rjvtechnology@gmail.com>
2022-11-18 00:06:30 +0300

    add Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "./components/UI/Container/Container";
 import Home from "./components/Web/Home/Home";
 import ReactGA from "react-ga";
-
 import "./App.scss";
 import {
   BrowserRouter as Router,
@@ -12,10 +11,14 @@ import {
 } from "react-router-dom";
 import CreateProfile from "./components/Web/CreateProfile/CreateProfile";
 
-const TRACKING_ID = "UA-236811494-1";
-ReactGA.initialize(TRACKING_ID);
+const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS;
+
+ReactGA.initialize(TRACKING_ID!);
 
 function App() {
+  useEffect(()=>{
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  },[])
   return (
     <Router>
       <div className="App">
