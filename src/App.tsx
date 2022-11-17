83593767151925e60b8b633990a23c08f6736956
RoniJackVituli <rjvtechnology@gmail.com>
2022-11-17 23:50:14 +0300

    add Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Container from "./components/UI/Container/Container";
 import Home from "./components/Web/Home/Home";
+import ReactGA from "react-ga";
 
-import './App.scss';
+import "./App.scss";
 import {
   BrowserRouter as Router,
   // Navigate,
@@ -11,11 +12,13 @@ import {
 } from "react-router-dom";
 import CreateProfile from "./components/Web/CreateProfile/CreateProfile";
 
+const TRACKING_ID = "UA-236811494-1";
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
   return (
     <Router>
       <div className="App">
-
         <Container>
           <Routes>
             <Route path="/createProfile" element={<CreateProfile />} />
--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./Button.module.scss";
 import { Link } from "react-router-dom";
+import ReactGA from "react-ga";
 
 type Props = {
   children: React.ReactNode;
@@ -10,7 +11,14 @@ type Props = {
   href?: string;
   iconLeft?: any;
   iconRight?: any;
-  style? : React.CSSProperties;
+  style?: React.CSSProperties;
+};
+
+const useAnalyticsEventTracker = (category: any) => {
+  const eventTracker = (action = "test action", label = "test label") => {
+    ReactGA.event({ category, action, label });
+  };
+  return eventTracker;
 };
 
 const Button: React.FC<Props> = ({
@@ -21,11 +29,17 @@ const Button: React.FC<Props> = ({
   href,
   iconLeft,
   iconRight,
-  style
+  style,
 }) => {
+  const gaEventTracker = useAnalyticsEventTracker('');
+
   if (link) {
     return (
-      <div className={classes.btn + ` ${className}`} onClick={onClick} style={style}>
+      <div
+        className={classes.btn + ` ${className}`}
+        onClick={onClick}
+        style={style}
+      >
         <Link to={link} style={{ textDecoration: "none" }}>
           <span className={classes.iconLeft}>{iconLeft}</span>
           {children}
@@ -37,8 +51,17 @@ const Button: React.FC<Props> = ({
 
   if (href) {
     return (
-      <div className={classes.btn + ` ${className}`} onClick={onClick} style={style}>
-        <a href={href} style={{ textDecoration: "none" }} >
+      <div
+        className={classes.btn + ` ${className}`}
+        onClick={() => {
+          if (onClick) {
+            onClick();
+          }
+          gaEventTracker(children?.toLocaleString())
+        }}
+        style={style}
+      >
+        <a href={href} style={{ textDecoration: "none" }}>
           <span className={classes.iconLeft}>{iconLeft}</span>
           {children}
           <span className={classes.iconRight}>{iconRight}</span>
@@ -48,7 +71,11 @@ const Button: React.FC<Props> = ({
   }
 
   return (
-    <div className={classes.btn + ` ${className}`} onClick={onClick} style={style}>
+    <div
+      className={classes.btn + ` ${className}`}
+      onClick={onClick}
+      style={style}
+    >
       <span className={classes.iconLeft}>{iconLeft}</span>
       {children}
       <span className={classes.iconRight}>{iconRight}</span>
